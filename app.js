02a4bd1aed4ae6d56550bfdf4c64d70bbc5423bd
vinicius.kirst@gmail.com <>
2014-01-24 13:00:29 +0000

    getters & setters

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-var app;
+/**
+ * 
+ */
+
+var language = "PHP";
+var caseType = "camel";
+var documentation = false;
+
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+	
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1618, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1618/');
+
+
+
+function generateGetter(propertyName)
+{
+	var srcCode = "function get" + propertyName + "() { return $this->" + propertyName + "; }";
+	return srcCode;
+}
+
+function generateSetter(propertyName)
+{
+	var srcCode = "function set" + propertyName + "($" + propertyName + ") { $this->" + propertyName + " = $" + propertyName +"; }";
+	return srcCode;
+}
